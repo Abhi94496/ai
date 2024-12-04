7791d10aea67075f053c819d2421ceee629bd053
Abhi94496 <133668056+Abhi94496@users.noreply.github.com>
2024-12-04 16:42:51 +0530

    Update openai_prompt.py

--- openai_prompt.py
+++ openai_prompt.py
@@ -1,1 +1,18 @@
+import openai as OpenAI
+import os
+
+os.environ['API_KEY'] = 'xyz'
+openai.api_key = os.getenv('API_KEY')
+client = OpenAI(api_key = os.environ.get('API_KEY'))
+
+promt = 'roast me by previous prompts of mine'
+
+response = client.chat.completions.create(messages=[ 
+                                                { 'role' : "user",
+                                                    'content' : promt,
+                                                      'max_tokens' : 300 }],
+                                          model = "gpt-3.5-turbo")
+
+print(response.choices[0].message.content)
+
 
